8bbfea71aaef00e997de93ff81a2fde01a08d16f
Waxmatuct <alvheim@rambler.ru>
2021-08-09 22:00:45 +0300

    create views for resource

--- resources/views/dashboard/plan.blade.php
+++ resources/views/dashboard/plan.blade.php
@@ -8,21 +8,43 @@
         <div class="container mx-auto sm:px-6 lg:px-8">
 
             @if ($books->isNotEmpty())
-            <div class="w-full mx-auto overflow-auto rounded-lg shadow-2xl bg-white">
+            <div class="w-full mx-auto overflow-auto rounded-lg shadow-2xl bg-white text-gray-700">
                 <table class="table-auto w-full text-xs">
                     <thead class="bg-gray-300 text-gray-800">
                         <tr>
-                            <th class="text-center py-3 px-4">№ в плане</th>
-                            <th class="text-center py-3 px-4">Факультет</th>
-                            <th class="text-center py-3 px-4">Авторы</th>
-                            <th class="text-center py-3 px-4">Наименование рукописи</th>
-                            <th class="text-center py-3 px-4">Вид издания</th>
-                            <th class="text-center py-3 px-4">Дисциплина</th>
-                            <th class="text-center py-3 px-4">Объем, уч.изд. л</th>
-                            <th class="text-center py-3 px-4">Тираж</th>
-                            <th class="text-center py-3 px-4">Месяц сдачи</th>
-                            <th class="text-center py-3 px-4">Сдано</th>
-                            <th class="text-center py-3 px-4 whitespace-nowrap">Статус **</th>               
+                            <th class="text-center py-3 px-4">
+                                № в плане
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Факультет
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Авторы
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Наименование рукописи
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Вид издания
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Дисциплина
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Объем, уч.изд. л
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Тираж
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Месяц сдачи
+                            </th>
+                            <th class="text-center py-3 px-4">
+                                Сдано
+                            </th>
+                            <th class="text-center py-3 px-4 whitespace-nowrap">
+                                Статус **
+                            </th>               
                         </tr>
                     </thead>
                     <tbody>
@@ -30,17 +52,41 @@
                         @foreach ($books as $book)
                         
                         <tr class="@if ($loop->even) bg-gray-100 @endif hover:bg-indigo-100">
-                            <th class="text-center py-3 px-4">{{ $book->item }}</th>
-                            <td class="text-center py-3 px-4">{{ $book->faculty->short_title }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->author }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->title }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->type->title }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->disciple }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->size }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->amount }}</td>
-                            <td class="text-center py-3 px-4">{{ $book->month->name }}</td>
-                            <td class="text-center py-3 px-4 text-green-500 text-lg">@if ($book->is_handed == 1) ✔ @endif</td>
-                            <td class="text-center py-3 px-4">@if ($book->is_handed == 1) {{ $book->status }} @endif</td>
+                            <th class="text-center py-3 px-4">
+                                <a href="{{ route('dashboard.book.edit', $book->id) }}" title="Правка">
+                                    {{ $book->item }}
+                                </a>
+                            </th>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->faculty->short_title }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->author }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->title }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->type->title }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->disciple }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->size }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->amount }}
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                {{ $book->month->name }}
+                            </td>
+                            <td class="text-center py-3 px-4 text-green-500 text-lg">
+                                @if ($book->is_handed == 1) ✔ @endif
+                            </td>
+                            <td class="text-center py-3 px-4">
+                                @if ($book->is_handed == 1) {{ $book->status }} @endif
+                            </td>
                         </tr>
                         
                         @endforeach
