f8745922cd0b56903784786011b3a76fd7cc43dc
Waxmatuct <alvheim@rambler.ru>
2021-12-29 17:26:06 +0000

    yandex verification

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="yandex-verification" content="1e910acb743c310a" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="@yield('description')">
 
