707a60d736fd69828cd71505b6da33de1feebfe3
Waxmatuct <alvheim@rambler.ru>
2022-04-12 18:11:08 +0000

    новые документы + бекап бд апрель 2022

--- database/seeders/FacultySeeder.php
+++ database/seeders/FacultySeeder.php
@@ -16,56 +16,67 @@ class FacultySeeder extends Seeder
     {
         DB::table('faculties')->insert([
             [
+                'id' => 1,
                 'title' => 'Факультет туризма и сервиса',
                 'short_title' => 'ФТС',
                 'slug' => 'fts',
+                'deleted_at' => null,
             ],
             [
+                'id' => 2,
                 'title' => 'Инженерно-экологический факультет',
                 'short_title' => 'ИЭФ',
                 'slug' => 'ief',
+                'deleted_at' => null,
             ],
             [
+                'id' => 3,
                 'title' => 'Факультет экономики и процессов управления',
                 'short_title' => 'ФЭиПУ',
                 'slug' => 'feipu',
                 'deleted_at' => '2022-02-01 00:00:00',
             ],
             [
+                'id' => 4,
                 'title' => 'Социально-педагогический факультет',
                 'short_title' => 'СПФ',
                 'slug' => 'spf',
+                'deleted_at' => null,
             ],
             [
+                'id' => 5,
                 'title' => 'Юридический факультет',
                 'short_title' => 'ЮФ',
                 'slug' => 'uf',
                 'deleted_at' => '2022-02-01 00:00:00',
             ],
             [
+                'id' => 6,
                 'title' => 'Университетский экономико-технологический колледж',
                 'short_title' => 'УЭТК',
                 'slug' => 'college',
+                'deleted_at' => null,
             ],
             [
-                'title' => 'Факультет инфомационных технологий и математики',
-                'short_title' => 'ФИТиМ',
-                'slug' => 'fitim',
-            ],
-            [
+                'id' => 7,
                 'title' => 'Факультет информационных технологий и математики',
                 'short_title' => 'ФИТиМ',
                 'slug' => 'fitim',
+                'deleted_at' => null,
             ],
             [
+                'id' => 8,
                 'title' => 'Факультет экономики и права',
                 'short_title' => 'ФЭиП',
                 'slug' => 'feip',
+                'deleted_at' => null,
             ],
             [
+                'id' => 99,
                 'title' => '-',
                 'short_title' => '-',
                 'slug' => 'none',
+                'deleted_at' => null,
             ],
         ]);
     }
