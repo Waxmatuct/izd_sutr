e8d8ce6912229e92e4a5d579cc5ed5e04a9994fb
Waxmatuct <alvheim@rambler.ru>
2022-04-17 18:14:05 +0300

    отправка плана по понедельникам

---
+++ app/Console/Commands/SendPlanByEmail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\SendPlan;
+use App\Services\BooksService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendPlanByEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'plan:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Отправляет план на текущий месяц';
+
+    /**
+     * booksService
+     *
+     * @var mixed
+     */
+    protected $booksService;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        BooksService $booksService,
+    ) {
+        parent::__construct();
+        $this->booksService = $booksService;
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $year = date('Y');
+        $plan = $this->booksService->getBooksOfCurrentMonth();
+        $stats = $this->booksService->getStatsOfYear($year);
+// dd($stats);
+// Отправляем план ...
+
+        Mail::to('izd-sgu@yandex.ru')->send(new SendPlan($plan, $stats));
+
+    }
+}
