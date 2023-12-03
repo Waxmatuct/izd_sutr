7e022f71187261cf6ae67821ab8fb03e79b9a5df
Waxmatuct <alvheim@rambler.ru>
2023-12-03 21:43:09 +0300

    переход на Laravel 10

--- config/backup.php
+++ config/backup.php
@@ -18,7 +18,7 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    base_path(),
+//                    base_path(),
                 ],
 
                 /*
@@ -166,12 +166,12 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'alvheim@rambler.ru',
-
-            'from' => [
-                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-                'name' => env('MAIL_FROM_NAME', 'Example'),
-            ],
+//            'to' => 'alvheim@rambler.ru',
+//
+//            'from' => [
+//                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+//                'name' => env('MAIL_FROM_NAME', 'Example'),
+//            ],
         ],
 
         'slack' => [
