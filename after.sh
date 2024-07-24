447cafccd460eeb51d626de37623b4e760db21e2
Waxmatuct <alvheim@rambler.ru>
2024-07-24 17:08:31 +0300

    homestead 15

--- after.sh
+++ after.sh
@@ -13,11 +13,11 @@
 # uncomment these lines to roll back to a previous version
 
 # Remove current Node.js version:
-#sudo apt-get -y purge nodejs
-#sudo rm -rf /usr/lib/node_modules/npm/lib
-#sudo rm -rf //etc/apt/sources.list.d/nodesource.list
+sudo apt-get -y purge nodejs
+sudo rm -rf /usr/lib/node_modules/npm/lib
+sudo rm -rf //etc/apt/sources.list.d/nodesource.list
 
 # Install Node.js Version desired (i.e. v13)
 # More info: https://github.com/nodesource/distributions/blob/master/README.md#debinstall
-#curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-#sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt-get install -y nodejs
